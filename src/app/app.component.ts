c81f125527ef568ac79d15d33c645109da942598
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2024-01-11 20:34:29 +0100

    slider without touch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,NavBarComponent,RouterOutlet],
+  imports: [CommonModule,NavBarComponent,RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
