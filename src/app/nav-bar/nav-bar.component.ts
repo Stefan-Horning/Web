a32c88c322175277fd66a03eca36ce2ca62d3d92
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2024-01-27 20:04:55 +0100

    nav

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  currentUrl?: string;
 
+  constructor(private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.route.url.subscribe(urlSegments => {
+      // Der letzte Segmente des URL-Pfads ist das, was Sie suchen
+      this.currentUrl = urlSegments[urlSegments.length - 1].path;
+      if(this.currentUrl == ''){
+        this.currentUrl = 'HOME';
+      }
+      console.log(this.currentUrl);
+    });
+  }
 }
