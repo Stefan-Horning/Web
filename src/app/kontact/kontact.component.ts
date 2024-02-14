bbd65b321d01e621967780238869c6e9029b58c2
Stefan-Horning <stefanhorning02704@gmail.com>
2024-02-14 18:10:19 +0100

    fix

--- src/app/kontact/kontact.component.ts
+++ src/app/kontact/kontact.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-kontact',
@@ -13,30 +14,45 @@ export class KontactComponent {
   send:boolean = false;
   name:any;
   email:any;
-  massage:any;
+  message:any;
   contactForm = new FormGroup({
     nameInput: new FormControl('', [Validators.required, Validators.minLength(2)]),
     emailInput: new FormControl('', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.(de|com|net|nl|org|uk|cn|au|dk|pl|cz|at|lu|ru)$')]),
     massageInput: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
 
+
+  http = inject(HttpClient)
+
+  post = {
+    endPoint: 'https://swetlana-makeupartist.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   async sendMail(){
-    let fd = new FormData();
-    fd.append('name',this.name);
-    fd.append('email',this.email);
-    fd.append('massage',this.massage);
-    try {
-      await fetch('https://formspree.io/f/moqgevkd', { 
-        method: 'POST', 
-        body: fd, 
-        mode: 'no-cors'
-      });
-      this.send = true;
-      setTimeout(() =>{
-        this.send = false;
-      },1000)
-    } catch (error) {
+    let contactData = {
+      name: this.name,
+      email:this.email,
+      message:this.message
     }
+    this.http.post(this.post.endPoint, this.post.body(contactData))
+        .subscribe({
+          next: (response) => {
+            this.name = '';
+            this.email = '';
+            this.message = '';
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
   }
 
   checkDisabeld(){
